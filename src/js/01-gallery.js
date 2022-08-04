1ff72bea5ad43ad1a520bdbe6c5e41e01809b721
Alona-Liubchenko <aylin888@rambler.ru>
2022-08-05 00:50:18 +0300

    h4

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const container = document.querySelector('.gallery');
+
+const cardMarkup = createGalleryMarkup(galleryItems);
+container.insertAdjacentHTML('beforeend', cardMarkup);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(item => {
+      return `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
